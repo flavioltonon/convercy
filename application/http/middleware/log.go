402a08efab544b8033bd02cbff27a04aeedabe44
Flávio Tonon <flavioltonon@gmail.com>
2023-05-04 21:46:02 -0300

    feat: add response_time to middleware logs

--- application/http/middleware/log.go
+++ application/http/middleware/log.go
@@ -2,6 +2,7 @@ package middleware
 
 import (
 	"net/http"
+	"time"
 
 	"convercy/domain/valueobject"
 	"convercy/shared/logging"
@@ -13,7 +14,10 @@ import (
 func Log(logger logging.Logger) mux.MiddlewareFunc {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			requestID := valueobject.GenerateRequestID()
+			var (
+				now       = time.Now()
+				requestID = valueobject.GenerateRequestID()
+			)
 
 			logger.Info("Got request",
 				logging.String("http_method", r.Method),
@@ -28,6 +32,7 @@ func Log(logger logging.Logger) mux.MiddlewareFunc {
 			logger.Info("Returning response",
 				logging.Stringer("request_id", requestID),
 				logging.Int("http_status_code", rw.Status()),
+				logging.Stringer("response_time", time.Since(now)),
 			)
 		})
 	}
